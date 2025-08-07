5e24e3d52c076f4fa1a8d29694370a9cc4a29cd5
Santiago Buitrago Giraldo <117487196+Kiofu7w7@users.noreply.github.com>
2025-08-06 21:40:44 -0500

    configuracion prisma swagger
    Se configuro prisma y swagger funcionando actualmente

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Base')
+    .setDescription('Documentación de la API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // <-- Montar Swagger antes de listen
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
